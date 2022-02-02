6ea54b81b518dd7a5a9b932fb541fc0b928c3b4f
Joe Grassel <jgrassel@us.ibm.com>
2022-02-02 10:46:37 -0600

    Add test project for JDK 18
    Signed-off-by: Joe Grassel <jgrassel@us.ibm.com>

--- io.openliberty.java.internal_fat_18/src/main/java/io/openliberty/java/internal/TestApp.java
+++ io.openliberty.java.internal_fat_18/src/main/java/io/openliberty/java/internal/TestApp.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2022 IBM Corporation and others.
+ * Copyright (c) 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,11 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
+package io.openliberty.java.internal;
 
-rootProject.name = 'open-liberty-misc'
-include 'io.openliberty.java.internal_fat_17'
-include 'io.openliberty.java.internal_fat_18'
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class TestApp extends Application {
+}
