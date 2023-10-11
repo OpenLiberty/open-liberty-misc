7f5d6254ccc3b49a767a3534e4e647139f12ae2b
Kyle Aure <KyleJAure@gmail.com>
2023-10-11 16:57:25 -0500

    Update CI to package .war and .jar archives

---
+++ .github/workflows/scripts/repackageApplication.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Exactly 2 arguments are required"
+    exit 0
+fi
+
+path=$(readlink -f $1) && echo "Input path: $path"
+ext=$2                 && echo "Input ext:  $ext"
+
+function repackage() {
+    lib=$(dirname $path)      && echo "Extract lib:  $lib"
+    file=$(basename $path)    && echo "Extract file: $file"
+    name="${file%.*}"         && echo "Extract name: $name"
+    
+    cp $path $lib/$name.$ext
+}
+
+repackage
