4e2afb7fd3e7aa155bef220a0e1f0eb2d4e17439
Greg Watts <gwatts@us.ibm.com>
2025-02-10 21:58:57 -0600

    Java 24 FAT test

--- .github/workflows/scripts/ClassVersionChecker.java
+++ .github/workflows/scripts/ClassVersionChecker.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2025 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 import java.io.*;
 import java.util.*;
 
@@ -35,6 +47,7 @@ public class ClassVersionChecker {
         majorCodeMap.put("21", 65);
         majorCodeMap.put("22", 66);
         majorCodeMap.put("23", 67);
+        majorCodeMap.put("24", 68);
 
         String filename = args[0];
         int expected = majorCodeMap.get(args[1]);
