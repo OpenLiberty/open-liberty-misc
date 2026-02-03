99d3ee3cc505eb99359454b50f254df6527a541e
Arunkumar K <Arunkumar.Kallyodan@ibm.com>
2026-02-03 15:39:59 +0530

    Additional updates

--- io.openliberty.java.internal_fat_26/src/main/java/io/openliberty/java/internal/TestService.java
+++ io.openliberty.java.internal_fat_26/src/main/java/io/openliberty/java/internal/TestService.java
@@ -58,55 +58,59 @@ public class TestService {
 		testFinalMeanFinal();
 
 		// Test HTTP/3 Support (JEP 517)
-	     testHTTP3Support();
+		testHTTP3Support();
 
 		log("Leaving testing");
 	}
 
 	// Prepare to Make Final Mean Final : JEP 500 -> https://openjdk.org/jeps/500
-	private void testFinalMeanFinal() {
-		log("Beginning JEP 500 testing");
+	private void testFinalMeanFinal() throws Exception {
+		log("Beginning JEP 500 testing: Prepare to Make Final Mean Final");
 
 		Person p = new Person();
 		log("Before mutation: " + p.name);
+		try {
+			// Attempt reflective mutation of final field
+			Field f = Person.class.getDeclaredField("name");
+			f.setAccessible(true);
+			f.set(p, "Mutated");
+			log("After mutation attempt: " + p.name);
 
-		// Attempt reflective mutation of final field
-		Field f = Person.class.getDeclaredField("name");
-		f.setAccessible(true);
-		f.set(p, "Mutated");
-		log("After mutation attempt: " + p.name);
+			// FAT validation
+			if (!"Original".equals(p.name)) {
+				throw new Exception("JEP 500 violation: final field was observably mutated via reflection");
+			}
 
-		// FAT validation
-		if (!"Original".equals(p.name)) {
-			throw new Exception("JEP 500 violation: final field was observably mutated via reflection");
+		} catch (NoSuchFieldException | IllegalAccessException e) {
+			log("Reflection access failed as expected: " + e.getMessage());
 		}
 
 		log("JEP 500 prepares the language for stricter final semantics in future releases");
 		log("Leaving JEP 500 testing");
 	}
 
-     // HTTP/3 for the HTTP Client API : JEP 517 -> https://openjdk.org/jeps/517
-    private void testHTTP3Support() {
-        log("Testing HTTP/3 Support (JEP 517)");
-        
-        try {
-            // Create an HTTP client with HTTP/3 support
-            HttpClient client = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_3)  // HTTP/3 support
-                .build();
-            
-            log("HTTP Client created with HTTP/3 support");
-            // This demonstrates that the API is available
-            log("HTTP/3 version: " + HttpClient.Version.HTTP_3);
-            
-            // Note: Actual HTTP/3 requests would require a server that supports HTTP/3
-            log("HTTP/3 is now available in the standard HttpClient API");
-            
-        } catch (Exception e) {
-            log("HTTP/3 test note: " + e.getMessage());
-            log("HTTP/3 support is available in Java 26 HttpClient API");
-        }
-    }
+	// HTTP/3 for the HTTP Client API : JEP 517 -> https://openjdk.org/jeps/517
+	private void testHTTP3Support() {
+		log("Testing HTTP/3 Support (JEP 517)");
+
+		try {
+			// Create an HTTP client with HTTP/3 support
+			HttpClient client = HttpClient.newBuilder().version(HttpClient.Version.HTTP_3) // HTTP/3 support
+					.build();
+
+			log("HTTP Client created with HTTP/3 support");
+			// This demonstrates that the API is available
+			log("HTTP/3 version: " + HttpClient.Version.HTTP_3);
+
+			// Note: Actual HTTP/3 requests would require a server that supports HTTP/3
+			log("HTTP/3 is now available in the standard HttpClient API");
+
+		} catch (Exception e) {
+			log("HTTP/3 test note: " + e.getMessage());
+			log("HTTP/3 support is available in Java 26 HttpClient API");
+		}
+        log("Leaving JEP 517 testing");
+	}
 
 	public void log(String msg) {
 		System.out.println(msg);
